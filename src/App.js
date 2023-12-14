4761682d15ae8a5e2841f44bcd8dc0dda0423251
BrainGhost <juniorbalamage@gmail.com>
2023-12-14 17:13:47 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React BrainGhost
+          Learn React wh BrainGhost
         </a>
       </header>
     </div>
