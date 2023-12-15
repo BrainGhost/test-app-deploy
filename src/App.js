0c508382820d824e83319060763c0a803c0ea631
BrainGhost <juniorbalamage@gmail.com>
2023-12-15 10:29:58 +0300

    git

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React wh BrainGhost
+          Learn React with BrainGhost
         </a>
       </header>
     </div>
