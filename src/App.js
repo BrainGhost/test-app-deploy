5bed9bc64cb6058e146129fc3812a786f52fb61b
BrainGhost <juniorbalamage@gmail.com>
2023-12-15 13:16:39 +0300

    git

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with BrainGhost
+          Learn Go from Balibonera junior
         </a>
       </header>
     </div>
